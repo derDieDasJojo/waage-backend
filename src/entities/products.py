e315746ca1cdc253feeef79d028773c070758655
Jojo <Jojo>
2022-11-01 20:42:54 +0100

    added flask

--- src/entities/products.py
+++ src/entities/products.py
@@ -1,9 +1,18 @@
 # coding=utf-8
 
+from marshmallow import Schema, fields
+
 from sqlalchemy import Column, String
 
 from .entity import Entity, Base
 
+class ProductSchema(Schema):
+    id = fields.Number()
+    name = fields.Str()
+    description = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
 
 class Product(Entity, Base):
     __tablename__ = 'exams'
--- src/main.py
+++ src/main.py
@@ -1,28 +1,68 @@
 # coding=utf-8
-
+from flask import Flask, jsonify, request
 from .entities.entity import Session, engine, Base
-from .entities.products import Product
+from .entities.products import Product, ProductSchema
 
-# generate database schema
+# creating the Flask application
+app = Flask(__name__)
+
+# if needed, generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-products = session.query(Product).all()
+@app.route('/products')
+def get_products():
+    # fetching from the database
+    session = Session()
+    product_objects = session.query(Product).all()
 
-if len(products) == 0:
-    # create and persist mock exam
-    python_exam = Product("Salat", "z.B. Feldsalat", "script")
-    session.add(python_exam)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = ProductSchema(many=True)
+    exams = schema.dump(product_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(exams.data)
 
-    # reload exams
-    products = session.query(Product).all()
 
-# show existing exams
-print('### Products:')
-for product in products:
-    print(f'({product.id}) {product.name} - {product.description}')
+@app.route('/products', methods=['POST'])
+def add_products():
+    # mount exam object
+    posted_product = ProductSchema(only=('name', 'description'))\
+        .load(request.get_json())
+
+    product = Product(**posted_product.data, created_by="HTTP post request")
+
+    # persist exam
+    session = Session()
+    session.add(product)
+    session.commit()
+
+    # return created exam
+    new_exam = ProductSchema().dump(product).data
+    session.close()
+    return jsonify(new_exam), 201
+
+# # generate database schema
+# Base.metadata.create_all(engine)
+#
+# # start session
+# session = Session()
+#
+# # check for existing data
+# products = session.query(Product).all()
+#
+# if len(products) == 0:
+#     # create and persist mock exam
+#     python_exam = Product("Salat", "z.B. Feldsalat", "script")
+#     session.add(python_exam)
+#     session.commit()
+#     session.close()
+#
+#     # reload exams
+#     products = session.query(Product).all()
+#
+# # show existing exams
+# print('### Products:')
+# for product in products:
+#     print(f'({product.id}) {product.name} - {product.description}')
